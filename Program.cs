12fecd63357be05694870d3cd731220882180c2c
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:35:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um valor que represente Δd e Δt para ser feito a velocidade média de um carro.");
+            Console.Write("Δd:");
+            double Δd=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Δt:");
+            double Δt=Convert.ToDouble(Console.ReadLine());
+            double resultado=Δd / Δt;
+            Console.Clear();
+            Console.WriteLine($"Velocidade média do carro é= {resultado} m/s");
+        }
+    }
+}
